e5b3d55e286ccd81b947ed4928cffe3d812aead6
kali-ninja <kali-ninja@kali-ninja.kali-ninja>
2025-04-10 21:55:55 +0200

    wip c11

--- listener.py
+++ listener.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import socket
 import json
+import base64
 
 class Listener:
     def __init__(self, ip, port):
@@ -27,7 +28,7 @@ class Listener:
 
     def write_file(self, path, content):
         with open(path, "wb") as file:
-            file.write(content)
+            file.write(base64.b64decode(content))
             return "[+] Download successful."
 
     def execute_remotely(self, command):
